83687286f263415182863a70735f317f0348316a
Oussama Hichou <usama.hicho.mail@gmail.com>
2023-10-07 19:05:57 +0100

    update

--- README.md
+++ README.md
@@ -1,40 +1,146 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# NASA Image Search App
 
-## Getting Started
+The NASA Image Search App is a web application that allows users to search for NASA imagery based on keywords and view the search results in a grid. This README provides a detailed explanation of the code used to build this app.
 
-First, run the development server:
+## Technologies Used
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+- React: A JavaScript library for building user interfaces.
+- JavaScript: The programming language used for app logic.
+- Tailwind CSS: A utility-first CSS framework for styling.
+
+## Code Explanation
+
+```jsx
+import React, { useState, useEffect } from 'react';
+
+const Index = () => {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // Number of items per page
+
+  const executeSearch = () => {
+    // Fetch data from the NASA API with the searchQuery
+    const apiUrl = `https://images-api.nasa.gov/search?q=${searchQuery}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.collection && data.collection.items) {
+          setData(data.collection.items);
+        } else {
+          // Handle the case where the API response does not match the expected structure.
+          console.error('Invalid API response:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('API request failed:', error);
+      });
+  };
+
+  // Pagination logic
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSearch = () => {
+    executeSearch();
+    setCurrentPage(1); // Reset to the first page when a new search is initiated.
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-blue-700">NASA Image Search</h1>
+      <div className="mb-4 mt-4">
+        <input
+          type="text"
+          placeholder="Search for images..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="px-4 py-2 border border-gray-300 rounded-md w-2/3 text-black"
+          // Add text-black class to set the text color to black
+        />
+        <button
+          onClick={handleSearch}
+          className="ml-2 bg-blue-500 hover-bg-blue-700 text-white font-semibold py-2 px-4 rounded-md cursor-pointer"
+        >
+          Search
+        </button>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {currentItems.map((item, index) => (
+          <div key={index} className="border p-4">
+            <img src={item.links[0].href} alt={item.data[0].title} className="max-w-full" />
+            <p className="mt-2">{item.data[0].title}</p>
+          </div>
+        ))}
+      </div>
+      <div className="pagination mt-4">
+        {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={
+              currentPage === index + 1
+                ? 'bg-blue-500 hover-bg-blue-700 text-white font-semibold py-2 px-4 rounded-md cursor-pointer'
+                : 'bg-gray-200 hover-bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-md cursor-pointer'
+            }
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Index;
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- The code sets up the React component `Index` to build the user interface of the NASA Image Search App.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+- It uses state variables to manage data, the search query, and pagination.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+- The `executeSearch` function fetches data from the NASA API based on the search query. It handles API responses and errors.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+- Pagination logic calculates the items to display on the current page.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+- Users can search for images by entering keywords, and the "Search" button initiates the search.
 
-## Learn More
+- Search results are displayed as a grid of images with titles.
 
-To learn more about Next.js, take a look at the following resources:
+- Pagination controls allow users to navigate between pages of search results.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- Styling is applied using Tailwind CSS classes for responsiveness and visual appeal.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+## Usage
 
-## Deploy on Vercel
+1. Clone the project repository to your local machine.
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+2. Run `npm install` to install the necessary dependencies.
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+3. Start the development server with `npm run dev`.
+
+4. Open your web browser and access the app at `http://localhost:3000`.
+
+5. Enter keywords in the search bar and click the "Search" button to find NASA imagery.
+
+## Additional Features (Challenge)
+
+The app can be expanded by implementing the following features:
+
+1. Pagination for navigating through
+
+ multiple pages of search results.
+
+2. The ability to filter search results by image type (e.g., photos, videos).
+
+3. Improved user interface with better styling and responsiveness.
+
+4. Deployment to a hosting service of your choice.
+
+5. Error handling and feedback for cases where the API request fails.
+
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,87 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const Index = () => {
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // Number of items per page
 
-export default function Home() {
+  const executeSearch = () => {
+    // Fetch data from the NASA API with the searchQuery
+    const apiUrl = `https://images-api.nasa.gov/search?q=${searchQuery}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.collection && data.collection.items) {
+          setData(data.collection.items);
+        } else {
+          // Handle the case where the API response does not match the expected structure.
+          console.error('Invalid API response:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('API request failed:', error);
+      });
+  };
+
+  // Pagination logic
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSearch = () => {
+    executeSearch();
+    setCurrentPage(1); // Reset to the first page when a new search is initiated.
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <h1 className="text-3xl font-semibold text-blue-700">NASA Image Search</h1>
+      <div className="mb-4 mt-4">
+        <input
+          type="text"
+          placeholder="Search for images..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="px-4 py-2 border border-gray-300 rounded-md w-2/3 text-black"
+          // Add text-black class to set the text color to black
         />
+        <button
+          onClick={handleSearch}
+          className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md cursor-pointer"
+        >
+          Search
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid grid-cols-3 gap-4">
+        {currentItems.map((item, index) => (
+          <div key={index} className="border p-4">
+            <img src={item.links[0].href} alt={item.data[0].title} className="max-w-full" />
+            <p className="mt-2">{item.data[0].title}</p>
+          </div>
+        ))}
       </div>
-    </main>
-  )
-}
+      <div className="pagination mt-4">
+        {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={
+              currentPage === index + 1
+                ? 'bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md cursor-pointer'
+                : 'bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-md cursor-pointer'
+            }
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Index;
